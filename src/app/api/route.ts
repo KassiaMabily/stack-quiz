32b703f93f03b29e7039fb21e74d4f55870e0755
Kassia Fraga <kassiafraga7@gmail.com>
2024-12-27 22:53:28 -0300

    refactor: Remove unused endpoints

--- src/app/api/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-
-type Data = {
-  message: string;
-};
-
-export async function GET() {
-  return NextResponse.json<Data>({ message: 'Hello, Next.js!' });
-}
